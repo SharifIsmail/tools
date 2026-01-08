472ba8d5be14e893f18b8e29ca5c46eea91b2aea
SharifIsmail <sharifis.mailbox@gmail.com>
2026-01-08 08:42:49 +0100

    Revert "first implementation by google gemini"
    This reverts commit 9dc93ee170ee5b30e09ca59a01bff36115ed2bae.

--- personal-assistant/src/services/workerClient.ts
+++
@@ -1,6 +0,0 @@
-import * as Comlink from 'comlink';
-import { IVirtualFileSystem } from '../shared/types';
-import VFSWorker from '../worker/vfs.worker?worker';
-
-const worker = new VFSWorker();
-export const workerVFS = Comlink.wrap<IVirtualFileSystem>(worker);
--- personal-assistant/src/worker/vfs.worker.ts
+++
@@ -1,5 +0,0 @@
-import * as Comlink from 'comlink';
-import { SQLiteVFS } from './SQLiteVFS';
-
-const vfs = new SQLiteVFS();
-Comlink.expose(vfs);
